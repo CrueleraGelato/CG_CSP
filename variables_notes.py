e19fc64a460655d709d1646b7f88759025248092
CarreraGelato <carrera.galata@ucas-edu.net>
2026-09-09 13:52:08 -0600

    Variable notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+#CG. variables Notes 
+
+name = input("Tell me your name:")
+
+print("Rysand is the most handsome high lord") 
+print("Its a statment of fact") 
+print("Hello", name, "welcome to class!") 
