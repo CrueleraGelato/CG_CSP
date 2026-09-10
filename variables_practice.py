69c0d27a80366f7884410d8e755d8362ed7abb16
CarreraGelato <carrera.galata@ucas-edu.net>
2026-09-10 13:40:45 -0600

    variables practice

---
+++ variables_practice.py
@@ -0,0 +1,22 @@
+# CG, variables practice 
+a_name = "Aprilia" 
+
+number_one = "7"
+
+number_100 = "151"
+
+breakfast = "toast"
+
+favorite_color = "burgendy"
+
+school_name = "uvu"
+
+the_year = "2026"
+
+eye_color = "brown"
+
+age = "42"
+
+favorite_subject = "History"
+
+print(a_name, number_one, number_100, breakfast, favorite_color, school_name, the_year, eye_color, age, favorite_subject) 
