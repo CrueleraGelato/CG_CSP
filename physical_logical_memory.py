8586970fe50f6fa869227614174731eaeba5af95
CarreraGelato <carrera.galata@ucas-edu.net>
2026-09-11 13:35:37 -0600

    physical logical memory

---
+++ physical_logical_memory.py
@@ -0,0 +1,8 @@
+# CG, Physical and logical memory
+
+pi = 3.1415 
+
+everything = 42 
+
+print(f"the variable pi is {pi} and it is saved at {id(pi)} ")
+print(f"the variable pi is {everything} and it is saved at {id(everything)}")        
