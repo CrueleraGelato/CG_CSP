611a93c80b2fce3db9e18ee7917a3e4adc4d6846
CarreraGelato <carrera.galata@ucas-edu.net>
2026-09-15 13:42:53 -0600

    hello user

---
+++ hello_user.py
@@ -0,0 +1,10 @@
+# CG, hello user
+
+while True:
+    name = input("Tell me your name: ").title().strip()         
+    if name.isnumeric(): 
+        print("sorry that is not a name") 
+    else: 
+        break
+print(f"Hello {name}, that is a beautiful name") 
+    
