30caacfb2ed245d38d017baa178810b08c06318e
CarreraGelato <carrera.galata@ucas-edu.net>
2026-09-18 13:12:41 -0600

    your budget

--- your_budget.py
+++ your_budget.py
@@ -14,6 +14,8 @@ savings = float((income/10))
 
 spending = float()
 
+Left = float(income-rent+utilities+groceries+transportation)
+
 print(f"Your rent is {rent} and that is {int(rent/income*100)} of your income") 
 
 print(f"Your utilities is {utilities} and that is {int(utilities/income*100)} of your income") 
@@ -23,6 +25,10 @@ print(f"Your groceries is {groceries} and that is {int(groceries/income*100)} of
 print(f"Your transportation is {transportation} and that is {int(transportation/income*100)} of your income") 
  
 print(f"You should save {savings} that is 10% of your income") 
+
+print(f"You have {Left} of spending money each month!")
+
+
 
 
     
